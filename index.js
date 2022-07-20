0782a9e9742d9db4fdc90347dffa5c01927083aa
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-07-20 22:03:19 +0500

    Create Scchema and Save my Docment to Collection

--- index.js
+++ index.js
@@ -3,3 +3,38 @@ const mongoose=require('mongoose')
 mongoose.connect('mongodb://localhost/playground')
 .then(()=>console.log("connected to MongoDb"))
 .catch(err=>console.error("Could not connect",err))
+
+
+
+//Defineing Schema
+const courseSchema=new mongoose.Schema({
+    name:String,
+    auther:String,
+    tags:[String],
+    date:{type:Date, default:Date.now},
+    isPublished:Boolean
+})
+
+
+//compiling scheme to model which gives a class
+const Course=mongoose.model("Course",courseSchema)
+
+
+async function createCourse(){
+// creating object of my Course
+const course=new Course({
+    name:"React js",
+    auther: "Huzefa Tariq",
+    tags:["React Js","FrontEnd"],
+    isPublished:true
+})
+
+const result=await course.save()
+console.log(result)
+}
+
+createCourse()
+
+
+
+

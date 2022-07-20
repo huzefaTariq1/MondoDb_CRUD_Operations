8f384b39cda11d0edd9d44223452fdd3407b1f36
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-07-20 21:08:36 +0500

    make connection to MongoDb
    make connction to MongoDB by using Mongoose

---
+++ index.js
@@ -0,0 +1,5 @@
+const mongoose=require('mongoose')
+
+mongoose.connect('mongodb://localhost/playground')
+.then(()=>console.log("connected to MongoDb"))
+.catch(err=>console.error("Could not connect",err))
